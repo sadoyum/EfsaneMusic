b6d72e7f54566a1079a7c657a2ffb5937cb55605
sadoyum <92607984+sadoyum@users.noreply.github.com>
2022-06-22 16:55:23 +0300

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -198,8 +198,8 @@ async def play(_, message: Message):
             keyboard = InlineKeyboardMarkup(
         [
             [
-                InlineKeyboardButton("🚀 İnfo", url=f"https://t.me/efsanestar_bot"),
-                InlineKeyboardButton("📣 Kanal", url=f"https://t.me/Sohbetdestek"),
+                InlineKeyboardButton("🚀 İnfo", url=f"https://t.me/sohbetnobir"),
+                InlineKeyboardButton("📣 Kanal", url=f"https://t.me/sohbetnobir"),
             ],[
                 InlineKeyboardButton("📲 Kapat", callback_data="cls"),
             ],
@@ -263,8 +263,8 @@ async def play(_, message: Message):
         keyboard = InlineKeyboardMarkup(
         [
             [
-                InlineKeyboardButton("🚀 İnfo", url=f"https://t.me/efsanestar_bot"),
-                InlineKeyboardButton("📣 Kanal", url=f"https://t.me/Sohbetdestek"),
+                InlineKeyboardButton("🚀 İnfo", url=f"https://t.me/sohbetnobir"),
+                InlineKeyboardButton("📣 Kanal", url=f"https://t.me/sohbetnobir"),
             ],[
                 InlineKeyboardButton("📲 Kapat", callback_data="cls"),
             ],
